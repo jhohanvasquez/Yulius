709d273bcdd9dac252c9ae52ea00fe2a643c6a0e
jhohan alexander vasquez <jhohan25@hotmail.com>
2020-12-06 21:41:14 -0500

    Client Api Test

--- Yulius.WebClient/Yulius.Api.Helper/Datos/KeyConfig.cs
+++ Yulius.WebClient/Yulius.Api.Helper/Datos/KeyConfig.cs
@@ -13,15 +13,6 @@ namespace Yulius.Helper.Api
         public static bool AutentificadorUniversal = bool.Parse(ConfigurationManager.AppSettings["AutentificadorUniversal"]);
 
         public static string Url_Login_Register = "Login.Register";
-        public static string Url_Login_ConfirmEmail = "Login.ConfirmEmail";
-        public static string Url_Login_SendConfirmCode = "Login.SendConfirmCode";
-        public static string Url_Login_ChangePassword = "Login.ChangePassword";
-        public static string Url_Login_ForgotPassword = "Login.ForgotPassword";
-        public static string Url_Login_ResetPassword = "Login.ResetPassword";
-        public static string Url_Login_Cliente = "Login.Cliente";
-        public static string Url_Login_FindByUserName = "Login.FindByUserName";
-        public static string Url_Login_GetOTP = "Login.GetOTP";
-        public static string Url_Login_DeleteOTP = "Login.DeleteOTP";
         public static string Url_Login_Auth = "Login.ChangePassword";
 
         // Codigo del sitio al registrarse en el administrador del autentificador universal.
